bd20edc307178d523345924872ce0a22ceb0fa65
MohitBhatt724065-maddy <ambikamohit724065@gmail.com>
2025-10-17 21:29:57 +0545

    Pizza price prediction project using lineaer regression.

---
+++ Project_1/Pizza_Price_Pred.py
@@ -0,0 +1,27 @@
+# First prediction model using linear regression.
+import pandas as pd
+import numpy as np
+
+Dataframe = pd.read_csv('Datasets/pizza_data.csv')
+print(Dataframe.sample(20))
+
+print(Dataframe.info())
+
+print(Dataframe['Company'].nunique())
+
+Dataframe['Price']  = Dataframe['Price'].str.replace('$', "", regex=False).astype(float)
+print(Dataframe)
+
+print(Dataframe.info())
+
+df_encoded = pd.get_dummies(
+    Dataframe,
+    columns=['Company', 'Size', 'Type'],
+    drop_first=True
+)
+
+print(df_encoded.head())
+
+print(Dataframe['Type'].nunique())
+
+print(df_encoded.columns)
