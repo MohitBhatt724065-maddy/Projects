ad2fd57c0ad2ec19c875990e2465429f4931b765
MohitBhatt724065-maddy <ambikamohit724065@gmail.com>
2025-10-29 08:21:30 +0545

    Modification.

--- Project_3/Model.py
+++ Project_3/Model.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 Dataframe = pd.read_csv('/home/mohit-bhatt/Desktop/Projects/Projects/Datasets/Social_Network_Ads.csv')
 
-print(Dataframe.sample(10))
-
+# print(Dataframe.sample(10))
+print(Dataframe)
 X = Dataframe.iloc[:, 1:4]
 # print(X.sample(10))
 
@@ -14,3 +14,7 @@ y = Dataframe.iloc[:, 4]
 X = pd.get_dummies(X, columns=['Gender'],  drop_first=True)
 
 print(X.sample(10))
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
